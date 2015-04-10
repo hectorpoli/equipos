3b2c1446633ba2278e6cdbec0eea4a46ffb18f95
Hector Poli <hpoli@opsu.gob.ve>
2015-04-10 10:01:45 -0430

    Probando menus

--- src/Hap/SoporteBundle/Menu/Builder.php
+++ src/Hap/SoporteBundle/Menu/Builder.php
@@ -10,9 +10,14 @@ class Builder extends ContainerAware
     public function mainMenu(FactoryInterface $factory, array $options)
     {
         $menu = $factory->createItem('root');
-
-        $menu->addChild('Categoria', array('route' => '_categoria'));
-        $menu['Categoria']->addChild('Nueva', array('route' => '_nueva_categoria'));
+        $menu->setChildrenAttribute('class', 'nav');
+        
+        
+        $menu->addChild('Categoria', array('route' => '_categoria'))
+                ->setAttribute('dropdown', true)
+                ->setAttribute('icon', 'icon-list');
+        $menu['Categoria']->addChild('Nueva Categoria',array('route' => '_nueva_categoria'))
+                ->setAttribute('icon', 'icon-list');
 
 
         return $menu;
---
+++ src/Hap/SoporteBundle/Menu/MenuBuilder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Hap\SoporteBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class MenuBuilder
+{
+    private $factory;
+
+    /**
+     * @param FactoryInterface $factory
+     */
+    public function __construct(FactoryInterface $factory)
+    {
+        $this->factory = $factory;
+    }
+
+    public function createMainMenu(RequestStack $requestStack)
+    {
+        $menu = $this->factory->createItem('root');
+
+        $menu->addChild('Categoria', array('route' => '_categoria'));
+        // ... add more children
+
+        return $menu;
+    }
+}
